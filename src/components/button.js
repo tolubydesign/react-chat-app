1752f1cd5c6070fafc9aeae936a263e7be8c8604
Tolu Adesina <tolu.adesina79@gmail.com>
2019-10-02 13:20:29 +0200

    testing git pull requests

--- src/components/button.js
+++ src/components/button.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 // import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 
+console.log('this is a button');
 export default class ButtonComponent extends Component {
   render() {
     return <Button variant="contained" className='button start-chat-button'>{this.props.button}</Button>;
